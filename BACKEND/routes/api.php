1c911571825f49e4846ca30e1a3a3d224dd0ebec
SossouGahEzechiel <ezecsossougah@gmail.com>
2026-01-17 19:36:25 +0000

    Sécurisation des endpoints déjà implémentées

--- BACKEND/routes/api.php
+++ BACKEND/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\{
-    AuthController,
-    MissionController,
+	AuthController,
+	MissionController,
 	UserController,
 	VehicleController
 };
@@ -10,15 +10,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => _200(env("APP_NAME") . " API is running"));
 
-Route::controller(AuthController::class)->prefix('auth')->group(function() {
+Route::controller(AuthController::class)->prefix('auth')->group(function () {
 	Route::post('login', 'login');
 	Route::post('logout', 'logout')->middleware('auth:sanctum');
 });
 
 // Route::post('users/{user}', [UserController::class, 'updateImage']);
 
-Route::apiResources([
-	'vehicles' => VehicleController::class,
-	'missions' => MissionController::class,
-	'users' => UserController::class
-]);
+Route::middleware(['auth:sanctum'])->group(function () {
+	Route::apiResources([
+		'vehicles' => VehicleController::class,
+		'missions' => MissionController::class,
+		'users' => UserController::class
+	]);
+});
