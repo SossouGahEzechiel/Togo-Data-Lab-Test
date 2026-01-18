79bc0cd3c0b1afc95ab0d6a036b392d903d85f0b
SossouGahEzechiel <ezecsossougah@gmail.com>
2026-01-18 21:00:06 +0000

    Revert "Mise en place de éléments clés pour le front"
    This reverts commit edb2014f438ae14ddd893db5bb070516f2d56405.

--- FRONTEND/nuxt.config.ts
+++ FRONTEND/nuxt.config.ts
@@ -1,28 +1,5 @@
-import { defineNuxtConfig } from "nuxt/config";
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: "2025-07-15",
-	devtools: { enabled: true },
-
-	modules: ["@pinia/nuxt", "@nuxt/ui", "@nuxtjs/color-mode", "nuxt-icon"],
-
-	ui: {
-		tailwindConfig: {
-			configPath: "~/tailwind.config.ts",
-		},
-	},
-
-	colorMode: {
-		classSuffix: "",
-		preference: "system",
-		fallback: "light",
-	},
-
-	runtimeConfig: {
-		public: {
-			apiBaseUrl: process.env.VITE_API_URL || "http://localhost:8000",
-		},
-	},
-
-	css: ["~/assets/css/main.css"],
-});
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true }
+})
